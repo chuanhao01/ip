c7ee7b6e26581eba328d5268166659561cfef5f9
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2026-08-20 10:58:02 +0800

    feat ref: Refactor chatbot functions and add echo
    Previously, all functionality was in the main function. Refactored the
    greet into its own function and implemented the echo function to echo
    whatever the user inputs.

--- src/main/java/Friedberg.java
+++ src/main/java/Friedberg.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Friedberg {
+    private static final String name = "Friedberg";
     public static void main(String[] args) {
-        String name = "Friedberg";
+        Friedberg chatbot = new Friedberg();
+        chatbot.greet();
+        chatbot.echo();
+    }
+    public void greet(){
         String banner =
               "______     _          _ _                   \n"
             + "|  ___|   (_)        | | |                  \n"
@@ -11,9 +18,25 @@ public class Friedberg {
             + "                                        __/ |\n"
             + "                                       |___/ \n";
         System.out.println(banner);
-        System.out.println(String.format("Hello! I'm %s.", name));
+        System.out.println(String.format("Hello! I'm %s.", Friedberg.name));
         System.out.println("I am a chatbot beep boop, what can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye bye, see you again next time.");
+    }
+    public void echo(){
+        Scanner stdin = new Scanner(System.in);  // Create a Scanner object
+        boolean isBye = false;
+        while (!isBye){
+            String userInput = stdin.nextLine();
+            userInput = userInput.strip();
+            String output = userInput;
+            if (userInput.equals("bye")){
+                output = "Bye bye, see you again next time.";
+                isBye = true;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(output);
+            System.out.println("____________________________________________________________");
+        }
+        stdin.close();
     }
 }
