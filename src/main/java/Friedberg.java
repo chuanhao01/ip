ae67f3e92acc0ab2cbc1043c0bb584e442cdba76
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2026-08-20 10:44:24 +0800

    feat: Rename Duke chatbot to Friedberg
    For level-0 of the iP, we need to rename the Duke.java into my chatbot
    of Friedberg.java

---
+++ src/main/java/Friedberg.java
@@ -0,0 +1,19 @@
+public class Friedberg {
+    public static void main(String[] args) {
+        String name = "Friedberg";
+        String banner =
+              "______     _          _ _                   \n"
+            + "|  ___|   (_)        | | |                  \n"
+            + "| |_ _ __ _  ___  __| | |__   ___ _ __ __ _ \n"
+            + "|  _| '__| |/ _ \\/ _` | '_ \\ / _ \\ '__/ _` |\n"
+            + "| | | |   | |  __/ (_| | |_) |  __/ | | (_| |\n"
+            + "\\_| |_|   |_|\\___|\\__,_|_.__/ \\___|_|  \\__, |\n"
+            + "                                        __/ |\n"
+            + "                                       |___/ \n";
+        System.out.println(banner);
+        System.out.println(String.format("Hello! I'm %s.", name));
+        System.out.println("I am a chatbot beep boop, what can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye bye, see you again next time.");
+    }
+}
